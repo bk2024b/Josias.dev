31ff02454081909fe82dca23243c244491f35d68
bk <josiasboco@gmail.com>
2025-04-09 03:55:25 +0100

    liens des projets en live

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='mt-20 bg-gray-900 py-10'>
           <div className='text-center flex flex-col items-center'>
               <div className='mb-4'>
-                <Image src="/logo.png" alt="Josias Boco" width={80} height={40} />
+                <Image src="/josias.png" alt="Josias Boco" width={80} height={40} />
               </div>
               <div className='flex items-center justify-center gap-2'>
                 <FaEnvelope className="text-gray-300" />
@@ -18,17 +18,17 @@ const Footer = () => {
               <p className='text-gray-300'>© 2025 Josias Boco. Tous droits réservés.</p>
               <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                   <li>
-                    <a target='_blank' href="https://github.com/josiasboco" className='flex items-center gap-2 text-gray-300 hover:text-white transition-colors'>
+                    <a target='_blank' href="https://github.com/bk2024b" className='flex items-center gap-2 text-gray-300 hover:text-white transition-colors'>
                       <FaGithub /> GitHub
                     </a>
                   </li>
                   <li>
-                    <a target='_blank' href="https://linkedin.com/in/josiasboco" className='flex items-center gap-2 text-gray-300 hover:text-white transition-colors'>
+                    <a target='_blank' href="https://www.linkedin.com/in/josias-boco-70963a219?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className='flex items-center gap-2 text-gray-300 hover:text-white transition-colors'>
                       <FaLinkedin /> LinkedIn
                     </a>
                   </li>
                   <li>
-                    <a target='_blank' href="https://instagram.com/josiasboco" className='flex items-center gap-2 text-gray-300 hover:text-white transition-colors'>
+                    <a target='_blank' href="https://instagram.com/josias.webdev?igsh=MWdhcTMzOHBiMGp6aQ==" className='flex items-center gap-2 text-gray-300 hover:text-white transition-colors'>
                       <FaInstagram /> Instagram
                     </a>
                   </li>
