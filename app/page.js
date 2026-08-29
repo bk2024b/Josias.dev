d3ccbe405421c0a44c736ce9b200e9760bb5f51a
Josias BOCO <josiasboco@gmail.com>
2026-08-29 16:29:00 +0100

    feat: reshape homepage around client conversion

--- app/page.js
+++ app/page.js
@@ -1,24 +1,18 @@
 import { Hero } from "@/components/home/Hero";
-import { Stats } from "@/components/home/Stats";
+import { ServicesPreview } from "@/components/home/ServicesPreview";
 import { FeaturedProjects } from "@/components/home/FeaturedProjects";
-import { AboutJourney } from "@/components/home/AboutJourney";
-import { TechStack } from "@/components/home/TechStack";
-import { GithubActivity } from "@/components/home/GithubActivity";
-import { CurrentlyBuilding } from "@/components/home/CurrentlyBuilding";
-import { DailyTools } from "@/components/home/DailyTools";
+import { WhyWorkWithMe } from "@/components/home/WhyWorkWithMe";
+import { Process } from "@/components/home/Process";
 import { CTA } from "@/components/home/CTA";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
+      <ServicesPreview />
       <FeaturedProjects />
-      <AboutJourney />
-      <TechStack />
-      <GithubActivity />
-      <CurrentlyBuilding />
-      <DailyTools />
+      <WhyWorkWithMe />
+      <Process />
       <CTA />
     </>
   );
